28e303b2105c24d4b3ec396ee17b81750ad5ea82
John Otander <johnotander@gmail.com>
2014-11-20 23:39:40 -0700

    Add ember-faker

--- tests/dummy/app/models/foo.js
+++ tests/dummy/app/models/foo.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import faker from 'faker';
 
 var Foo = DS.Model.extend({
   name: DS.attr('string'),
@@ -10,7 +11,7 @@ var fixtures = [];
 for(var i = 0; i < 1000; i++) {
   fixtures.push({
     id: i,
-    name: Math.random().toString(),
+    name: faker.name.firstName() + ' ' + faker.name.lastName(),
     email: 'example' + i +'@example.com'
   });
 }
